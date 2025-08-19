7f3a1a7792ab0dc6250e19a66332ec5b32d2face
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-08-19 14:46:36 +0000

    Adicionando rota default.
    Definindo rota default.

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,22 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
-const PORT = 3000;
 
 app.use(express.json());
+dotenv.config();
+
+const PORT: number = parseInt(`${process.env.PORT || 3000}`);
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
     message: 'Api Agenda'
+  });
+});
+
+app.use((req: Request, res: Response) =>{
+  res.status(404).json({
+    message: 'Rota não encontrada'
   });
 });
 
