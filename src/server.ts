170d00c8d9d8865aa6af94c0e4ad9fb7b71bc7d1
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-08-22 10:53:14 +0000

    Iniciando o swagger.
    - iniciando o swagger na rota.
    - fazendo os primeiros comentários.

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,14 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import helmet from 'helmet';
 import permissionsPolicy from 'permissions-policy';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerOptions from './config/swaggerbase';
 import politics from './config/segure/politics';
 import pessoaRouter from './routes/pessoaRouter';
 
 const app = express();
+const specs = swaggerJsdoc(swaggerOptions); 
 
 app.use(express.json());
 app.use(permissionsPolicy({
@@ -21,14 +25,39 @@ dotenv.config();
 
 const PORT: number = parseInt(`${process.env.PORT || 3000}`);
 
+app.use('/api-docs', swaggerUi.serve,swaggerUi.setup(specs));
+
 app.use(pessoaRouter);
 
+
+/**
+ * @swagger
+ * /:
+ *  get:
+ *    sumary: Retorna rota principal.
+ *    description: Retorna rota principal.
+ *    responses:
+ *      200:
+ *        description: Sucesso
+ *        message: Api Agenda.
+ */
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
     message: 'Api Agenda'
   });
 });
 
+/**
+ * @swagger
+ * /Não encontrada:
+ *   get:
+ *    sumary: Rota nao encontrada.
+ *    description: Rota não encontrada.
+ *    responses:
+ *      404:
+ *        description: 404 not found.
+ *        message: Rota não encontrada.
+ */
 app.use((req: Request, res: Response) =>{
   res.status(404).json({
     message: 'Rota não encontrada'
